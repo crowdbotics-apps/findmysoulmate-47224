e204d465b3c8f82be16be22af16ba84eec8a3454
Crowdbotics <team@crowdbotics.com>
2024-03-22 10:47:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "findmysoulmate-47224",
+  url: "https://findmysoulmate-47224.botics.co",
+  api: "https://findmysoulmate-47224.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
